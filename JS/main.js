788874db458f6409bb4543f4a41c6b9e01873eac
Boryana Rakova <bubeto1q1@gmail.com>
2022-01-24 15:24:10 +0000

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+
+function plusSlides(n) {          //what is "n"?
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+   var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.lenght) {slideIndex = 1}              //i can't get that
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    } 
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += "active";
+}
+
+
+
